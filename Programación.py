6040fceaefcfa7f2cb762e24184625e22f7fd713
Emmanuel23-dev <cardonagomezemanuel@gmail.com>
2025-02-03 12:58:51 -0500

    Update Programación.py

--- Programación.py
+++ Programación.py
@@ -1,3 +1,13 @@
 print("Hola repositorio", end=" ")
 print("Este es un nuevo repositorio", end="!")
 x = int(input("What's x?"))
+def main():
+  Bricks_mario(size):
+
+
+def Bricks_mario(n):
+  for i in range(size):
+    for j in range(size):
+      print("#" * size , end="")
+    print()
+  main()
